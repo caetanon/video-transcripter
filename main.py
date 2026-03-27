48fdcb8d2b67d1f326bf3af783e8952055724559
Caetano Neto <caetano.n@gmail.com>
2026-03-27 14:06:09 -0300

    correção NoneType object has no attribute write

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+# Fix para PyInstaller no Windows: stdout/stderr são None em modo windowed
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, "w")
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, "w")
+
 from ui.app import App
 
 
